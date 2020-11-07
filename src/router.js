30babb1a92951e9cdb4b97ecc71f8ae4e1e58a06
Matthijs Halvemaan <matthijs.halvemaan@gmail.com>
2020-11-07 22:03:21 +0100

    add 404 page

--- src/router.js
+++ src/router.js
@@ -1,39 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Gents from './views/Gents.vue'
-import Ladies from './views/Ladies.vue'
+import Home from './views/Home.vue';
 
 Vue.use(Router)
 
 export default new Router({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: Home
+    // mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/gents',
+            name: 'gents',
+            component: () => import('./views/Gents.vue'),
+        },
+        {
+            path: '/ladies',
+            name: 'ladies',
+            component: () => import('./views/Ladies.vue'),
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('./views/About.vue'),
+        },
+        {
+            path: '/support',
+            name: 'support',
+            component: () => import('./views/Support.vue'),
 		},
 		{
-			path: '/gents',
-			name: 'gents',
-			component: Gents
-		},
-		{
-			path: '/ladies',
-			name: 'ladies',
-			component: Ladies
-		},
-		{
-			path: '/about',
-			name: 'about',
-			component: () => import('./views/About.vue')
-		},
-		{
-			path: '/support',
-			name: 'support',
-			component: () => import('./views/Support.vue')
+			path: '*',
+			name: '404',
+			component: () => import('./views/404.vue'),
 		}
-	]
-})
+    ],
+});
---
+++ src/views/404.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-container fluid grid-list-lg>
+        <v-layout wrap>
+            <v-flex xs12>
+                <v-row justify="center">
+                    <v-col md="6">
+                        <v-card color="orange">
+                            <v-card-title justify="center">
+                                404: Page could not be found
+                            </v-card-title>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-flex>
+            <v-flex xs6>
+				<v-card v-on:click="navigate('/')">
+					<v-card-title>
+						<p>Go to Home</p>
+					</v-card-title>
+				</v-card>
+			</v-flex>
+            <v-flex xs6>
+				<v-card v-on:click="navigate('about')">
+					<v-card-title>
+						<p>About this app</p>
+					</v-card-title>
+				</v-card>
+			</v-flex>
+            <v-flex xs6>
+                <v-card ripple v-on:click="navigate('gents')">
+                    <v-card-title center>
+                        <p>Soundboard Torius Gents</p>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+            <v-flex xs6>
+                <v-card ripple v-on:click="navigate('ladies')">
+                    <v-card-title>
+                        <p>Soundboard Torius Ladies</p>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+			<!-- <v-flex xs4>
+				<v-card v-on:click="navigate('support')">
+					<v-card-title>
+						<p>Support</p>
+					</v-card-title>
+				</v-card>
+			</v-flex> -->
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        components: {},
+        methods: {
+            navigate: function(route) {
+                this.$router.push(route);
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    h1 {
+        justify-content: center;
+        align-content: center;
+    }
+</style>
